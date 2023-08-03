54879c513c0359bcb3fa6317533827087b87e0c1
ozanakinci <ozan_akinci@outlook.com>
2023-08-03 16:57:38 +0300

    This is drawing example with using turtle

---
+++ main.py
@@ -0,0 +1,42 @@
+import turtle
+
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("light blue")
+drawing_board.title("Python Turtle")
+turtle_instance = turtle.Turtle()
+
+# turtle_instance.forward(100)
+# turtle_instance_2 = turtle.Turtle()
+# turtle_instance_2.left(90)
+# turtle_instance_2.forward(100)
+
+#square
+# turtle_instance = turtle.Turtle()
+# for i in range(4):
+#
+#     turtle_instance.forward(200)
+#     turtle_instance.left(90)
+#
+# turtle.done()
+
+#star
+# turtle_instance = turtle.Turtle()
+# for i in range(5):
+#     turtle_instance.forward(200)
+#     turtle_instance.right(144)
+# turtle.done()
+
+#polygon
+
+turtle_instance = turtle.Turtle()
+num_sides = 6
+angle = 360.0 / num_sides
+side_length = 100
+
+for i in range(num_sides):
+    turtle_instance.right(angle)
+    turtle_instance.forward(side_length)
+
+turtle.done()
+
+
